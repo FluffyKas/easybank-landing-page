9dbd7de70ae1f16dbb451568ba2702d55da1a39d
FluffyKas <maruzs.zso@gmail.com>
2022-01-19 11:27:58 +0100

    Mobile menu: changing markup from 2 separate buttons to one, adjusting javascript to it, adding styles for animation

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const hamburger_btn = document.querySelector(".hamburger-btn")
-const close_btn = document.querySelector(".close-btn")
 const header_nav = document.querySelector(".header-nav");
 
 hamburger_btn.addEventListener("click", () => {
-  header_nav.classList.add("active")
-  close_btn.classList.add("active")
-  hamburger_btn.classList.add("opened")
-})
-
-close_btn.addEventListener("click", () => {
-  header_nav.classList.remove("active")
-  close_btn.classList.remove("active")
-  hamburger_btn.classList.remove("opened")
+  header_nav.classList.toggle("active")
+  hamburger_btn.classList.toggle("active")
 })
