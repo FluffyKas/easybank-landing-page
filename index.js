8421a9cfbbdaee383531cf56f9737b8996a7028c
FluffyKas <maruzs.zso@gmail.com>
2022-05-03 13:12:48 +0200

    Added intersection observer

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const hamburger_btn = document.querySelector(".hamburger-btn")
+// Mobile menu
+
+const hamburger_btn = document.querySelector(".hamburger-btn");
 const header_nav = document.querySelector(".header-nav");
 
 hamburger_btn.addEventListener("click", () => {
   header_nav.classList.toggle("active")
   hamburger_btn.classList.toggle("active")
+})
+
+// Observer to reveal content on scroll
+
+const observedElements = document.querySelectorAll(".observed");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("reveal", entry.isIntersecting)
+    if (entry.isIntersecting) observer.unobserve(entry.target)
+  })
+}, {
+  threshold: .35
+})
+
+observedElements.forEach(element => {
+  observer.observe(element);
 })
